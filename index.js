f90cd55c8423aacc24d4dd21170c597780a4b375
Frank Lee <plat4m@outlook.com>
2022-04-02 11:21:47 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(list, name) {
+    return list.filter(function(driverName){
+        return driverName.toLowerCase() == name.toLowerCase();
+    });
+}
+
+
+
+function fuzzyMatch(list, name) {
+    let nameLength = name.length;
+    return list.filter(function(driverName){
+        return driverName.slice(0, nameLength) === name;
+    });
+}
+
+
+
+function matchName(list, name) {
+    return list.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
